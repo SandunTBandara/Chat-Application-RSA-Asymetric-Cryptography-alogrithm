e1da7c7caa0498b668ca2ec707d04e8f52d5262f
Sandun <94672855+SandunTBandara@users.noreply.github.com>
2023-05-01 18:04:43 +0530

    Add files via upload

---
+++ chatapplication/src/chatapplication/Chatapplication.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapplication;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+/**
+ *
+ * @author Sanu
+ */
+public class Chatapplication {
+
+    /**
+     * @param args the command line arguments
+     */
+
+}
